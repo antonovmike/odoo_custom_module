5f2ec287b216897028156af860b6b021d30fa1ea
antonovmike <antonovma@outlook.com>
2024-02-08 14:31:15 +0400

    new estate.property.type model

--- estate/models/estate_test.py
+++ estate/models/estate_test.py
@@ -34,4 +34,4 @@ class TestModel(models.Model):
         ],
         default='new'
     )
-    property_type_id = fields.Many2one('estate.property.type', string='Property Type', required=True)
+    property_type_id = fields.Many2one('estate.property.type', 'property_id')
